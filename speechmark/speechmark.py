ad9288cb8dcc03d2ec58d6b810c8b1355eaa1af0
tundish <tundish@gigeconomy.org.uk>
2025-05-05 10:52:45 +0100

    Update licencing.

--- speechmark/speechmark.py
+++ speechmark/speechmark.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 #   encoding: utf-8
 
-# This is part of the speechmark library.
-# Copyright (C) 2023 D E Haynes
+# Copyright (C) 2025 D E Haynes
+# This file is part of spiki.
 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-# USA
+# Spiki is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Spiki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with spiki.
+# If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 from collections import deque
