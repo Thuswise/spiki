d42101205cbc27b412e6a8a1c2137827647b7ad6
tundish <tundish@gigeconomy.org.uk>
2026-04-30 16:24:45 +0100

    Tinkering with example.

--- spiki/examples/eclectic/tabulator.py
+++ spiki/examples/eclectic/tabulator.py
@@ -15,14 +15,29 @@
 # You should have received a copy of the GNU General Public License along with spiki.
 # If not, see <https://www.gnu.org/licenses/>.
 
+
+from collections import UserDict
 import csv
 import logging
 from pathlib import Path
-from UsetDict import UserDict
 
 from spiki.plugin import Change
 from spiki.plugin import Plugin
 from spiki.renderer import Renderer
+
+
+class Derivation(UserDict):
+
+    @property
+    def sections(self):
+        try:
+            return self.data["doc"]["html"]["body"]["main"].get("section", [])
+        except KeyError:
+            return []
+
+    @property
+    def templates(self):
+        return {n: i for n, i in enumerate(self.sections) if i.get("derive")}
 
 
 class Tabulator(Plugin):
@@ -38,6 +53,22 @@ class Tabulator(Plugin):
         rv = super().__exit__(exc_type, exc_val, exc_tb)
         return rv
 
+    def run_ingest(self, path: Path = None, node: dict = None, doc: str = None, **kwargs) -> Change:
+        if path.suffix == ".csv":
+            with path.open(newline="") as data_file:
+                reader = csv.DictReader(data_file)
+                self.sources[path] = list(reader)
+
+        return Change(self, path=path, node=node, doc=doc)
+
     def run_extend(self, path: Path = None, node: dict = None, doc: str = None, **kwargs) -> Change:
-        self.logger.info("Hello")
+        d = Derivation(node)
+        for pos, section in d.templates.items():
+            path = self.visitor.location_of(node).parent.joinpath(section["derive"]).resolve()
+            if "blocks" in section:
+                for n, row in enumerate(self.sources.get(path, [])):
+                    print(f"{row=}")
+
+            self.logger.info(f"{path=}")
+
         return Change(self, path=path, node=node, doc=doc)
